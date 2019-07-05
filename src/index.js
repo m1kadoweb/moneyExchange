d73a4545b36f049f9736cb6a17a61b99f21b6e88
m1kadoweb <m1kadosgs@gmail.com>
2019-07-05 09:26:42 +0500

    Done 100%

--- src/index.js
+++ src/index.js
@@ -1,47 +1,54 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-	var h, q, d, n, p;
-	var i;
+module.exports = function makeExchange(currency) {		
+		var h = 0;
+		var q = 0;
+		var d = 0;
+		var n = 0;
+		var p = 0;		
+		var result = {};
+		
 if(currency >= 10000) {	
 	return {error: "You are rich, my friend! We don't have so much coins for exchange"};
 } else if(currency <= 0){
-	return {};
+	return result;
 } else if((currency > 0) && (currency < 10000)) {
 	
-	if(currency%50 != 0) {
-		h = Math.floor(currency/50);
-		if(Math.floor((currency%50)%25 != 0)) {
-			q = Math.floor((currency%50)/25);
-			if((currency - h*50 - q*25)%10 != 0) {
-				d = Math.floor((currency - h*50 - q*25)/10);
-			} else {
-				d = (currency - h*50 - q*25)/10;
-			}
-		} else {
-			q = currency/25;
+	
+		while(currency >= 50) {
+			currency -= 50;
+			h++;
+			result["H"] = h;
 		}
-	} else {
-		h = currency/50;
-	}
-	
-	
-} 
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-};
 
+		while(currency >= 25) {
+			currency -=25;
+			q++;
+			result["Q"] = q;
+		}
 
+		while(currency >= 10) {
+			currency -=10;
+			d++;
+			result["D"] = d;
+		}
 
+		while(currency >= 5) {
+			currency -=5;
+			n++;
+			result["N"] = n;
+		}
 
-// 9999  H = 199(50) - 9950(Остаток 49) 
-// 49    Q = 1(25)   - 25  (Остаток 24)
-// 24    D = 2(10)   - 20  (Остаток 4)
-// 4     P = 4(1)    - 4
-// 
+		while(currency >= 1) {
+			currency -=1;
+			p++;
+			result["P"] = p;
+		}
 
-/* h = Math.floor(currency/50);
-	q = Math.floor((currency%50)/25);
-	d = Math.floor((currency - h*50 - q*25)/10);
-	p = currency - h*50 - q*25 - d*10;
-	return {H: + h, Q: + q, D: + d, P: + p}; 
-	*/
+		
+
+				
+
+	return result;
+}
+}; 
+    
